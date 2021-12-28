e407b1ec4809fd168dede6d55dc8dd96243b40f5
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-28 11:35:28 +0900

    users 앱 생성 후 users 모델링(class User)

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    user_name  = models.CharField(max_length=50)
+    email      = models.CharField(max_length=100, unique=True)
+    password   = models.CharField(max_length=256)
+    mobile     = models.CharField(max_length=100, null=True)
+    address    = models.CharField(max_length=300, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'users'
