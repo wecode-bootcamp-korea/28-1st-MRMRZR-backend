8d3a92ebed04a33c257784275891f1c49bedaef9
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-28 15:31:45 +0900

    users app 생성 후 models.py에 class User 작성

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name  = models.CharField(max_length=50)
+    name       = models.CharField(max_length=50)
     email      = models.CharField(max_length=100, unique=True)
     password   = models.CharField(max_length=256)
     mobile     = models.CharField(max_length=100, null=True)
