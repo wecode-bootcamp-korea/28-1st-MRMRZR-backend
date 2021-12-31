bd846ba29d329091b8fecf00ae5d5e910e23841b
WONSUK JI <wonsukji@WONSUKs-MacBook-Pro.local>
2021-12-31 18:19:45 +0900

    tried to find errors in ProductDetailView

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
-from django import path
+from django.urls import path
 from products.views import ProductDetailView
 
 urlpatterns = [
-    path('/product_detail', ProductDetailView.as_view())
+    path('/detail', ProductDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 import json
-from django.db import models
 
 from django.http import JsonResponse
 from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import ValidationError
 from django.db.models import Sum, Q
 
@@ -10,30 +10,34 @@ from products.models import Category, Item, Product, Size, ProductImage, Product
 
 
 class ProductDetailView(View):
-    def get(self, request, product_id):
-        if not Product.objects.filter(id = product_id).exists():
-            return JsonResponse({'message' : 'DOES NOT EXIST'}, status=404)
+    def get(self, request):
+        try:
+            product_id = request.GET.get('id')
+            product = Product.objects.get(id = product_id) 
+            if not Product.objects.filter(id = product_id).exists():
+                return JsonResponse({'message' : 'DOES NOT EXIST'}, status=404)
             
-        # 딕셔너리 만들기 전 지정해주어야 하는 요소들 방식 질문!
-        product = Product.objects.get(id = product_id)
-        # Product.objects.all() ???
-        image = product.image_set.all()
-        size = product.size_set.all()
+               
+            # 딕셔너리 만들기 전 지정해주어야 하는 요소들 방식 질문!
+            # Product.objects.all() ???
+            #image = product.image_set.all()
+            #size = product.size_set.all()
+            #image = ProductImage.objects.get(product_id = product_id) #이 부분 질문 - url을 꺼내온다?
+            # 이미지 클래스에서 불러오기
+            #size = 
             
-        #image = ProductImage.objects.get(product_id = product_id) #이 부분 질문 - url을 꺼내온다?
-        # 이미지 클래스에서 불러오기
-        #size = 
-            
-        result = {
-            'id' : product.id,
-            'name' : product.name,
-            'product_number' : product.product_name,
-            'description' : product.description,
-            'price' : product.price,
-            'is_new' : product.is_new,
-            'image_url' : [product.image_url for product in image],
-            'size' : size.name,
+            result = {
+                'id' : product.id,
+                'name' : product.name,
+                'product_number' : product.product_name,
+                'description' : product.description,
+                'price' : product.price,
+                'is_new' : product.is_new,
+                #'image_url' : [product.image_url for product in image],
+                #'size' : size.name,
             }               
-        }
-            
-        return JsonResponse({'results' : result}, status = 200)
+                
+            return JsonResponse({'results' : result}, status = 200)
+        
+        except Product.DoesNotExist:
+            return JsonResponse({'message' : 'NOT_FOUND'}, status=401)
