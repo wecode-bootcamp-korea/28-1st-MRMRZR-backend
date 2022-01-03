1e18c00c675350df1fcb1b8b9df3260373d66eed
Bruno-Jang <sosdnrud@gmail.com>
2022-01-03 12:03:19 +0900

    Modify - 카트 제품 조회 기능 추가

--- carts/views.py
+++ carts/views.py
@@ -13,11 +13,9 @@ class CartView(View):
             data = json.loads(request.body)
             #user = request.user    유저에게 토큰 전달한것 인증되면 활성화해서 테스트
             user = User.objects.get(id=data['user_id'])
-            product = Product.objects.get(id=data['product_id'])    # id와 name 중에 name이 더 직관적이지만 우리가 데이터를 일일이 볼게 아니기도 하고 id가 조금이라도 메모리 공간?을 덜 잡아먹어서 id로 값을 보내는걸까?
+            product = Product.objects.get(id=data['product_id'])
             size = Size.objects.get(id=data['size_id'])
             quantity = data['quantity']
-            
-            # print('user : ', user)
             
             if not Product.objects.filter(id=product.id).exists():
                 return JsonResponse({'message': 'INVALID_PRODUCT'}, status=400)
@@ -26,29 +24,32 @@ class CartView(View):
                 return JsonResponse({'message': 'INVALID_SIZE'}, status=400)
             
             product_option = ProductOption.objects.filter(product_id=product, size_id=size)
-
-            # print('productoption : ', product_option[0].id)
-
-            # print('productoption[0] - stock : ', product_option[0].stock)
-
-            if not quantity >= 1 or quantity <= int(product_option[0].stock):
+            print(product_option[0].stock)
+            print(quantity)
+            
+            if quantity < 1 or quantity > int(product_option[0].stock):
                 return JsonResponse({'message': 'INVALID_QUANTITY'}, status=400)
 
-            if Cart.objects.filter(user_id=user).exists():
+            if Cart.objects.filter(products_options=product_option).exists():       # get_or_create 참고 Product 테이블에 sizes라는 MTMF throug = ProductOption 
+                Cart.objects.create(
+                    products_options = product_option,
+                    quantity = quantity,
+                )
+                return JsonResponse({'message': 'SUCESS ADD'}, status=201)
+            else:
+                
                 cart = Cart.objects.create(
+                    user_id = user,
+                    products_options = product_option[0],
                     quantity = quantity
                 )
-                cart.quantity += quantity
+                print(cart.quantity)
                 cart.save()
-                return JsonResponse({'result': 'SUCCESS'}, status=201)
-            
-            cart = Cart.objects.create(
-                user_id = user,
-                products_options = product_option[0],
-                quantity = quantity
-            )
-            #cart.quantity += quantity
-            cart.save()
-            return JsonResponse({'message': 'SUCCESS'}, status=201)
+                return JsonResponse({'message': 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'message': 'KeyError'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'DoesNotExist'}, status=401)
+
+    def get(self, request, product_id):
+        user = User.objects.get(id=product_id)
