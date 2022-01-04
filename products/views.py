a504a6b7a47ef048fe0a5f14d5efc84fa43715cb
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-01-04 18:42:32 +0900

    mentor review

--- products/views.py
+++ products/views.py
@@ -11,34 +11,61 @@ from products.models import Category, Item, Product, Size, ProductImage, Product
 # 이름, 가격, imgurl, size,
 class ProductListView(View):
     def get(self, request):
-        products = Product.objects.filter()
-        images   = [image.url for image in product.productimage_set.all()]
-        sizes    = [size.name for size in product.productoption_set.all()]
-        order_condition = request.GET.get('order', None)
+        """
+        1. 조건의 맞는 product list을 전달해 주는 것이 목적
+        2. 조건이 무엇있는가
+        => catrgory, item, size, 높은 가격순, 낮은 가격순, 페이지네이션
 
-        if order_condition == 'price_ascending':
-            products = Product.objects.order_by('price')
+        """
+        1. 프론트에서 온 값들 점검
+        2. 가공이 필요한 로직
+        3. 내가 응답할 데이터 
 
-        if order_condition == 'price_descending':
-            products = Product.objects.order_by('-price')
+        
+        category = request.GET.get('category', None)
+        item     = request.GET.get('item', None)
+        size     = request.GET.getlist('size', None) # => [X, L, XL]
+        sort     = request.GET.get('sort', "id")
+        offset   = int(request.GET.get('offset', 0))
+        limit    = int(request.GET.get('limit', 5))
+        
+        """
+        size 쿼리 파라미터 2가지 방법
+        1. :8000/products?item=abc&sort=price&size=X&size=L&size=XL
+        => request.GET.getlist('size', None) # => [X, L, XL]
+        
+        2. :8000/products?item=abc&sort=price&size=X,L,XL
+            request.GET.get('size', None) # => X,L,XL
+            sizes = size.split(',') => X, L, XL]
+        """
+        
+        q = Q(productoption__stock__gt=0)
 
-        products = [{
-            'id'            : product.id,
+        if category:
+            q &= Q(item__category__name=category)
+        
+        if item:
+            q &= Q(item__name=item)
+        
+        if size:
+            q &= Q(sizes__in=size) #size는 list여야 한다.
+            
+        sort_set = {
+            "id"     : "id",
+            "price"  : "price",
+            "-price" : "-price"
+        }
+
+        results = [{
+            'product_id'    : product.id,
             'name'          : product.name,
             'product_number': product.product_number,
             'price'         : product.price,
             'is_new'        : product.is_new,
-            'item'          : product.item,
-            "images"        : images,
-            "sizes"         : sizes,
-        } for product in products
-        ]
-        
-        
-        results = {
-            'products' : products
-        }
-
+            'item'          : product.item.name,
+            "image"         : product.productimage_set.first()
+        } for product in Product.objects.filter(q).order_by(sort_set[sort])[offset:offset+limit]]
+    
         return JsonResponse({"results" : results}, status = 200)
 
 """
