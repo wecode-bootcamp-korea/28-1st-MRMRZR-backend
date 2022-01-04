f7b309e5a34131008c5afb1121506e050324c327
Bruno-Jang <sosdnrud@gmail.com>
2022-01-04 16:48:25 +0900

    장바구니에 제품 추가 기능 WIP

--- carts/views.py
+++ carts/views.py
@@ -11,49 +11,46 @@ class CartView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            #user = request.user    유저에게 토큰 전달한것 인증되면 활성화해서 테스트
-            user = User.objects.get(id=data['user_id'])
-            product = Product.objects.get(id=data['product_id'])
-            size = Size.objects.get(id=data['size_id'])
-            quantity = data['quantity']
+            # user = request.user    유저에게 토큰 전달한것 인증되면 활성화해서 테스트
+            # user = get_object_or_404(User, user=request.user)
+            user             = User.objects.get(pk=data['user_id'])
+            product          = Product.objects.get(id=data['product_id'])
+            size             = Size.objects.get(id=data['size_id'])
+            product_quantity = data['quantity']
+            product_option   = ProductOption.objects.get(product_id=product, size_id=size)
             
-            if not Product.objects.filter(id=product.id).exists():
-                return JsonResponse({'message': 'INVALID_PRODUCT'}, status=400)
-            
-            if not Size.objects.filter(id=size.id).exists():
-                return JsonResponse({'message': 'INVALID_SIZE'}, status=400)
-            
-            product_option = ProductOption.objects.get(product_id=product, size_id=size)
-            print(product_option)
-            
-            if quantity < 1 or quantity > int(product_option.stock):
+            if product_quantity < 1 or product_quantity > int(product_option.stock):
                 return JsonResponse({'message': 'INVALID_QUANTITY'}, status=400)
 
-            if Cart.objects.filter(products_options__in=product_option).exists():       # get_or_create 참고
-                cart = Cart.objects.create(
-                    user_id = user.id,
-                    products_options = product_option[0],
-                    quantity = quantity,
+            if Cart.objects.filter(user=user, products_options=product_option).exists():
+                current_quantity = Cart.objects.get(user=user, products_options=product_option).quantity
+                cart, is_created = Cart.objects.update_or_create(
+                    user_id=user,
+                    products_options_id=product_option,
+                    defaults= {'quantity': int(product_quantity) + current_quantity}
                 )
-                cart.quantity += quantity
-                cart.save()
-                return JsonResponse({'message': 'SUCESS_CART_TO_ADD'}, status=201)
-            
+                return JsonResponse({'message': 'SUCCESS'}, status=201)
+                
             else:
-                print(user)
+                # get_or_create 참고
                 cart = Cart.objects.create(
                     user_id = user.id,
                     products_options = product_option,
-                    quantity = quantity
+                    quantity = product_quantity
                 )
-                print(cart.quantity)
-                cart.save()
                 return JsonResponse({'message': 'SUCCESS'}, status=201)
+            
         except KeyError:
             return JsonResponse({'message': 'KeyError'}, status=400)
         except User.DoesNotExist:
-            return JsonResponse({'message': 'DoesNotExist'}, status=401)
-
+            return JsonResponse({'message': 'User_DoesNotExist'}, status=401)
+        except Product.DoesNotExist:
+            return JsonResponse({'message': 'Product_DoesNotExist'}, status=401)
+        except Size.DoesNotExist:
+            return JsonResponse({'message': 'Size_DoesNotExist'}, status=401)
+        except ProductOption.DoesNotExist:
+            return JsonResponse({'message': 'ProductOption_DoesNotExist'}, status=401)
+        
     def get(self, request, user_id):
         try:
             user  = User.objects.get(pk=user_id)
