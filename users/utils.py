c08b9273e6c3e9b28fa5db7cc9c4aa2822e3f60b
WONSUK JI <wonsukji@WONSUKs-MacBook-Pro.local>
2022-01-05 15:30:05 +0900

    created LogInDecorator in utils.py in the users app

---
+++ users/utils.py
@@ -0,0 +1,28 @@
+import jwt
+
+from functools              import wraps
+from django.http            import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from users.models           import User
+from my_settings            import SECRET_KEY, ALGORITHM
+
+def LogInDecorator(func):
+    @wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        if not request.headers.get('Authorization'):
+            return JsonResponse({'message' : 'NO_TOKEN'}, status = 403)
+        
+        try:
+            access_token = request.headers.get('Authorization', None)
+            decoded_token = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+            request.user = User.objects.get(id = decoded_token['user_id'])
+            
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 403)
+        
+        except ObjectDoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status = 403)
+        
+        return func(self, request, *args, **kwargs)
+    return wrapper
