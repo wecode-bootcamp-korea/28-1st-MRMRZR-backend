6a30a7ac1f25e2fbf689c6e63dd4c2a2b1b42a70
WONSUK JI <wonsukji@WONSUKs-MacBook-Pro.local>
2022-01-05 18:11:35 +0900

    edited LogInDecorator

--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,5 @@
 import jwt
 
-from functools              import wraps
 from django.http            import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -8,15 +7,14 @@ from users.models           import User
 from my_settings            import SECRET_KEY, ALGORITHM
 
 def LogInDecorator(func):
-    @wraps(func)
     def wrapper(self, request, *args, **kwargs):
-        if not request.headers.get('Authorization'):
-            return JsonResponse({'message' : 'NO_TOKEN'}, status = 403)
-        
         try:
-            access_token = request.headers.get('Authorization', None)
+            if not request.headers.get('Authorization'):
+                return JsonResponse({'message' : 'NO_TOKEN'}, status = 403)
+            
+            access_token  = request.headers.get('Authorization', None)
             decoded_token = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
-            request.user = User.objects.get(id = decoded_token['user_id'])
+            request.user  = User.objects.get(id = decoded_token['user_id'])
             
         except jwt.exceptions.DecodeError:
             return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 403)
